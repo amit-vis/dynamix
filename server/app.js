480d5482d2112e28a1c82ac19b3d29f412c78595
amit-vis <amit200698@gmail.com>
2024-12-23 22:05:21 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { connectDB } = require('./config/database');
 const checklistRoutes = require('./routes/checklistRoutes');
 
 const app = express();
-
-connectDB();
 
 app.use(cors());
 app.use(express.json());
